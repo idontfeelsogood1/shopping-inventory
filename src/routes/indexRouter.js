a80fb223252991fa395cdb6b7a4a536daab69187
Le Duong <leduongnewmemes123@gmail.com>
2025-09-23 13:10:44 +0700

    feat: Finish basic route structure

---
+++ src/routes/indexRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const route = Router()
+
+const indexController = require('../controllers/indexController')
+route.get('/', indexController.indexGet)
+
+const itemController = require('../controllers/itemController')
+route.get('/items', itemController.allItemGet)
+route.get('/items/category/:category_id', itemController.categoryItemGet)
+route.post('/items/create', itemController.createItemPost)
+route.post('/items/:item_id/update', itemController.updateItemPost)
+route.post('/items/:item_id/remove', itemController.removeItemPost)
+
