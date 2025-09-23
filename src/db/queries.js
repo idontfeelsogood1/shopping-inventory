b6f618fdf3be35804e237112bfebd3be20811d0d
Le Duong <leduongnewmemes123@gmail.com>
2025-09-23 14:33:52 +0700

    feat: getAllItems now return rows with item_id and category_id

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,7 +1,7 @@
 const db = require('./pool')
 
 async function getAllItems() {
-    const { rows } = await db.query(`SELECT item.id, category.id, item.name, price, img_path, category.name AS category_name
+    const { rows } = await db.query(`SELECT item.id AS item_id, category.id AS category_id, item.name, price, img_path, category.name AS category_name
                                      FROM item 
                                      JOIN category ON category.id = item.category_id`)
     return rows
