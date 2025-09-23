6b5e6bf7078c70b6aa6f8e41374e7653e36b3638
Le Duong <leduongnewmemes123@gmail.com>
2025-09-23 15:13:10 +0700

    feat: Finish implementing categoryItemGet

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,10 +1,18 @@
 const db = require('../db/queries')
 
 async function allItemGet(req, res) {
-    const items = await db.getAllItems()
-    res.render('items', { items: items })
+    const arr = await db.getAllItems()
+    res.render('items', { items: arr })
+}
+
+async function categoryItemGet(req, res) {
+    const id = req.params.category_id
+    const name = await db.getCategoryName(id)
+    const arr = await db.getCategoryItems(id)
+    res.render('category', { items: arr, category_name: name })
 }
 
 module.exports = {
     allItemGet,
+    categoryItemGet,
 }
