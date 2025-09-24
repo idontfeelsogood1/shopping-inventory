f5693ffb88f3e838abc1c0c2ba3ea934dfd2c860
Le Duong <leduongnewmemes123@gmail.com>
2025-09-24 15:40:33 +0700

    feat: Configure multer to handle receiving binary image data

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,5 +1,20 @@
 const { Router } = require('express')
 const route = Router()
+const path = require('path')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', 'public', 'item_images'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const ext = path.extname(file.originalname)
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 const indexController = require('../controllers/indexController')
 route.get('/', indexController.indexGet)
@@ -8,8 +23,9 @@ const itemController = require('../controllers/itemController')
 route.get('/items', itemController.allItemGet)
 route.get('/items/category/:category_id', itemController.categoryItemGet)
 route.get('/items/create', itemController.createItemGet)
-route.post('/items/create', itemController.createItemPost)
-route.get('/items/:item_id/update', itemController.updateItemGet)
-route.post('/items/:item_id/update', itemController.updateItemPost)
-route.post('/items/:item_id/remove', itemController.removeItemPost)
+route.post('/items/create', upload.single('image'), itemController.createItemPost)
+// route.get('/items/:item_id/update', itemController.updateItemGet)
+// route.post('/items/:item_id/update', itemController.updateItemPost)
+// route.post('/items/:item_id/remove', itemController.removeItemPost)
 
+module.exports = route
